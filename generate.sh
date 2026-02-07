2e040920aa2b3f312ae11446689419f20ee423ae
LTI2000 <timo.koepke@googlemail.com>
2026-02-07 21:35:17 +0100

    Patch generated files to prevent build errors

--- generate.sh
+++ generate.sh
@@ -6,9 +6,15 @@ openapi3-code-generator-exe                \
   --module-name OpenAI.Generated           \
   --do-not-generate-stack-project
 
+# Fix build errors in CreateChatCompletion.hs:
+# The generated code uses Data.Aeson.Decoding.eitherDecodeStrict
+# which doesn't exist in the aeson library.
+# Replace it with the correct function Data.Aeson.eitherDecodeStrict.
+sed -i '' 's/Data\.Aeson\.Decoding\.eitherDecodeStrict/Data.Aeson.eitherDecodeStrict/g' \
+  tmp/src/OpenAI/Generated/Operations/CreateChatCompletion.hs
+
 rm -rf src/OpenAI/Generated*
 
 cp -r tmp/src .
 
 rm -rf tmp
-
